17d6e29c186376a075bf43ae86a404c1a683c361
Roger Gonzalez <roger.fernando1901@gmail.com>
2026-09-10 19:31:52 -0600

    feat (login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //Control para mostrar u ocultar la contraseña
+  bool _obscure = true;
   @override
   Widget build(BuildContext context) {
     //Para obtener el tamaño de la pantalla
@@ -23,6 +25,44 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: size.width,
                 height: 200,
                 child: RiveAnimation.asset('assets/login-bear.riv'),
+              ),
+              //para separar espacios
+              SizedBox(height: 10),
+              TextField(
+                //Para mostrar el tipo de teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              //Campo para contraseña
+              TextField(
+                //Para mostrar el tipo de teclado
+                obscureText: _obscure,
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    //Operador Ternario
+                    icon: Icon(
+                      _obscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      //Refrescar
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
             ],
           ),
