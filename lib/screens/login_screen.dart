f3d9a351d752375acdf41d5c16008c5bae7919e9
Roger Gonzalez <roger.fernando1901@gmail.com>
2026-09-17 20:44:03 -0600

    feat(rive): wire up isChecking/isHandsUp inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
 class LoginScreen extends StatefulWidget {
-  const new({super.key});
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //Variable para el control de la visibilidad de la contraseña
+  bool _obscure = true;
+
+  //1.1 crear el cerebro de la animacion
+  StateMachineController? _controller;
+  //SMI: State Machine Input / Entradas de la maquina de estados
+  SMIBool? _isChecking;
+  SMIBool? _isHandsUp;
+  SMITrigger? _trigSuccess;
+  SMITrigger? _trigFail;
+
   @override
   Widget build(BuildContext context) {
     //Para obtener el tamaño de la pantalla
@@ -22,7 +33,89 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(
                 width: size.width,
                 height: 200,
-                child: RiveAnimation.asset('assets/login-bear.riv'),
+                child: RiveAnimation.asset(
+                  'assets/login-bear.riv',
+                  stateMachines: ['Login Machine'],
+                  //Vincular animaccion
+                  onInit: (artboard) {
+                    _controller = StateMachineController.fromArtboard(
+                      artboard,
+                      'Login Machine',
+                    );
+
+                    //1.3 Verificar que inicio bien
+                    if (_controller == null) return;
+                    //Agregamos el controlador al escenario
+                    artboard.addController(_controller!);
+                    //Vinculamos variables
+                    _isChecking = _controller?.findSMI('isChecking');
+                    _isHandsUp = _controller?.findSMI('isHandsUp');
+                    _trigSuccess = _controller?.findSMI('trigSuccess');
+                    _trigFail = _controller?.findSMI('trigFail');
+                  },
+                ),
+              ),
+              //para separar espacio
+              SizedBox(height: 10),
+              //Campo de texto para email
+              TextField(
+                onChanged: (value) {
+                  if (_isHandsUp != null) {
+                    //No tapes los ojos al ver el email
+                    _isHandsUp?.change(false);
+                  }
+                  //Si isChecking no es nulO
+                  if (_isChecking != null) {
+                    //Activar el modo chismoso
+                    _isChecking!.change(true);
+                  }
+                },
+                //para mostrar el tipo de teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    //para redondear los bordes
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              //Campo de texto para contraseña
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (value) {
+                  if (_isChecking != null) {
+                    //No tapes los ojos al ver el email
+                    _isChecking?.change(false);
+                  }
+                  //Si isChecking no es nulO
+                  if (_isHandsUp != null) {
+                    //Activar el modo chismoso
+                    _isHandsUp!.change(true);
+                  }
+                },
+                obscureText: _obscure,
+                //para mostrar el tipo de teclado
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      //Refrescar el estado
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    //para redondear los bordes
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
             ],
           ),
